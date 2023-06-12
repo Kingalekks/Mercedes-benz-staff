0f9f35ae04754ca383eb3eeb303df6d688c02a3f
Alexander King <95908181+Kingalekks@users.noreply.github.com>
2023-06-12 14:49:03 +0100

    Add files via upload

---
+++ Objects and Classes (Employees Mercedes-Benz Group).py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[85]:
+
+
+class consultant:
+    def _init_(employee):
+        employee.name = "Smith Moses"
+        employee.sex = "Male"
+        employee.age = 26
+        employee.job_title = "Business Consultant"
+        employee.id = "00126"
+        employee.salary = "$10.21 hourly"
+        employee.yearly_salary = "$25,000"
+            
+    def work(employee):
+        print("Name: Smith Moses")
+        print("Sex: Male")
+        print("Age: 26")
+        print("Job Title: Business Consultant")
+        print("Staff ID: 00126")
+        print("Salary: $10.21 hourly")
+        print("Yearly salary: $25,000")
+        print("Smith Moses is a staff of Mercedes-Benz Group")
+    
+obj=consultant()
+obj.work()
+
+
+# In[84]:
+
+
+class consultant:
+    def _init_(employee):
+        employee.name = "Taiwo Oladele"
+        employee.sex = "Male"
+        employee.age = 30
+        employee.job_title = "Business Consultant"
+        employee.id = "00111"
+        employee.salary = "$10.21 hourly"
+        employee.yearly_salary = "$25,000"
+            
+    def work(employee):
+        print("Name: Taiwo Oladele")
+        print("Sex: Male")
+        print("Age: 30")
+        print("Job Title: Business Consultant")
+        print("Staff ID: 00111")
+        print("Salary: $10.21 hourly")
+        print("Yearly salary: $25,000")
+        print("Taiwo Oladele is a staff of Mercedes-Benz Group")
+    
+obj=consultant()
+obj.work()
+
+
+# In[83]:
+
+
+class consultant:
+    def _init_(employee):
+        employee.name = "Veronica Mark"
+        employee.sex = "Female"
+        employee.age = 31
+        employee.department = "Business Consultant"
+        employee.id = "00129"
+        employee.salary = "$10.21 hourly"
+        employee.yearly_salary = "$25,000"
+            
+    def work(employee):
+        print("Name: Veronica Mark")
+        print("Sex: Female")
+        print("Age: 31")
+        print("Job Title: Business Consultant")
+        print("Staff ID: 00129")
+        print("Salary: $10.21 hourly")
+        print("Yearly salary: $25,000")
+        print("Veronica Mark is a staff of Mercedes-Benz Group")
+    
+obj=consultant()
+obj.work()
+
+
+# In[82]:
+
+
+class consultant:
+    def _init_(employee):
+        employee.name = "Jessica Anoai"
+        employee.sex = "Female"
+        employee.age = 30
+        employee.job_title = "Business Consultant"
+        employee.id = "00133"
+        employee.salary = "$10.21 monthly"
+        employee.yearly_salary = "$25,000"
+            
+    def work(employee):
+        print("Name: Jessica Anoai")
+        print("Sex: Female")
+        print("Age: 30")
+        print("Job Title: Business Consultant")
+        print("Staff ID: 00133")
+        print("Salary: $10.21 hourly")
+        print("Yearly salary: $25,000")
+        print("Jessica Anoai is a staff of Mercedes-Benz Group")
+    
+obj=consultant()
+obj.work()
+
+
+# In[81]:
+
+
+class consultant:
+    def _init_(employee):
+        employee.name = "Michelle Timothy"
+        employee.sex = "Female"
+        employee.age = 35
+        employee.job_title = "Business Consultant"
+        employee.id = "00093"
+        employee.salary = "$10.21 monthly"
+        employee.yearly_salary = "$25,000"
+            
+    def work(employee):
+        print("Name: Michelle Timothy")
+        print("Sex: Female")
+        print("Age: 35")
+        print("Job Title: Business Consultant")
+        print("Staff ID: 00093")
+        print("Salary: $10.21 hourly")
+        print("Yearly salary: $25,000")
+        print("Michelle Timothy is a staff of Mercedes-Benz Group")
+    
+obj=consultant()
+obj.work()
+
+
+# In[80]:
+
+
+class bizdevmgr:
+    def _init_(employee):
+        employee.name = "Alister Gates"
+        employee.sex = "Male"
+        employee.age = 32
+        employee.job_title = "Business Development Manager"
+        employee.id = "00093"
+        employee.salary = "$36.01 monthly"
+        employee.yearly_salary = "$83,774"
+            
+    def work(employee):
+        print("Name: Alister Gates")
+        print("Sex: Male")
+        print("Age: 32")
+        print("Job Title: Business Development Manager")
+        print("Staff ID: 00093")
+        print("Salary: $36.01 hourly")
+        print("Yearly salary: $83,774")
+        print("Alister Gates is a staff of Mercedes-Benz Group")
+    
+obj=bizdevmgr()
+obj.work()
+
+
+# In[79]:
+
+
+class manager:
+    def _init_(employee):
+        employee.name = "Sonia Scott"
+        employee.sex = "Female"
+        employee.age = 37
+        employee.job_title = "Manager"
+        employee.id = "00144"
+        employee.salary = "$40.49 monthly"
+        employee.yearly_salary = "$130,000"
+            
+    def work(employee):
+        print("Name: Sonia Scott")
+        print("Sex: Female")
+        print("Age: 37")
+        print("Job Title: Manager")
+        print("Staff ID: 00144")
+        print("Salary: $40.49 hourly")
+        print("Yearly salary: $130,000")
+        print("Sonia Scott is a staff of Mercedes-Benz Group")
+    
+obj=manager()
+obj.work()
+
+
+# In[77]:
+
+
+class manager:
+    def _init_(employee):
+        employee.name = "Adams Kenneth"
+        employee.sex = "Male"
+        employee.age = 37
+        employee.job_title = "Manager"
+        employee.id = "00252"
+        employee.salary = "$40.49 monthly"
+        employee.yearly_salary = "$130,000"
+            
+    def work(employee):
+        print("Name: Adams Kenneth")
+        print("Sex: Male")
+        print("Age: 37")
+        print("Job Title: Manager")
+        print("Staff ID: 00252")
+        print("Salary: $40.49 hourly")
+        print("Yearly salary: $130,000")
+        print("Adams Kenneth is a staff of Mercedes-Benz Group")
+    
+obj=manager()
+obj.work()
+
+
+# In[76]:
+
+
+class servmgr:
+    def _init_(employee):
+        employee.name = "Adedayo Gentry"
+        employee.sex = "Male"
+        employee.age = 35
+        employee.job_title = "Service Manager"
+        employee.id = "00252"
+        employee.salary = "$27.72 monthly"
+        employee.yearly_salary = "$84,379"
+            
+    def work(employee):
+        print("Name: Adedayo Gentry")
+        print("Sex: Male")
+        print("Age: 35")
+        print("Job Title: Service Manager")
+        print("Staff ID: 00252")
+        print("Salary: $27.72 hourly")
+        print("Yearly salary: $84,379")
+        print("Adedayo Gentry Kenneth is a staff of Mercedes-Benz Group")
+    
+obj=servmgr()
+obj.work()
+
+
+# In[78]:
+
+
+class dirofops:
+    def _init_(employee):
+        employee.name = "John Haastrup"
+        employee.sex = "Male"
+        employee.age = 43
+        employee.job_title = "Director of Operations"
+        employee.id = "00065"
+        employee.salary = "$97.75 monthly"
+        employee.yearly_salary = "$119,863"
+            
+    def work(employee):
+        print("Name: John Haastrup")
+        print("Sex: Male")
+        print("Age: 43")
+        print("Job Title: Director of Operations")
+        print("Staff ID: 00065")
+        print("Salary: $97.75 hourly")
+        print("Yearly salary: $119,863")
+        print("John Haastrup is a staff of Mercedes-Benz Group")
+    
+obj=dirofops()
+obj.work()
+
+
+# In[86]:
+
+
+class supervisor:
+    def _init_(employee):
+        employee.name = "Caroline Johnson"
+        employee.sex = "Female"
+        employee.age = 35
+        employee.job_title = "Supervisor"
+        employee.id = "00100"
+        employee.salary = "$36.05 monthly"
+        employee.yearly_salary = "$114,502"
+            
+    def work(employee):
+        print("Name: Caroline Johnson")
+        print("Sex: Female")
+        print("Age: 35")
+        print("Job Title: Supervisor")
+        print("Staff ID: 00100")
+        print("Salary: $36.05 hourly")
+        print("Yearly salary: $114,502")
+        print("Caroline Johnson is a staff of Mercedes-Benz Group")
+    
+obj=supervisor()
+obj.work()
+
+
+# In[92]:
+
+
+class director:
+    def _init_(employee):
+        employee.name = "Esther James"
+        employee.sex = "Female"
+        employee.age = 37
+        employee.job_title = "Director"
+        employee.id = "00129"
+        employee.salary = "$73.95 monthly"
+        employee.yearly_salary = "$114,502"
+            
+    def work(employee):
+        print("Name: Esther James")
+        print("Sex: Female")
+        print("Age: 37")
+        print("Job Title: Director")
+        print("Staff ID: 00129")
+        print("Salary: $73.95 hourly")
+        print("Yearly salary: $114,502")
+        print("Esther James is a staff of Mercedes-Benz Group")
+    
+obj=director()
+obj.work()
+
+
+# In[ ]:
+
+
+
+
